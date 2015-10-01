f186a7297eae21d1cda7cad0aa5055a3d53c0194
Madda <nobody.youknow@local>
2015-10-01 21:02:30 +0100

    Add team5

--- teams/__init__.py
+++ teams/__init__.py
@@ -2,6 +2,7 @@ from .team4 import Team4Bot
 from .team1Green import PlayerGreen1, PlayerGreen2
 from .agressivetitfortat import AggressiveTitForTaTeam6
 from .team1partred import Team1DarkRed
+from .team5 import Prescient, Fucker, Sane
 
 players = [ 
     Team4Bot,
@@ -9,4 +10,7 @@ players = [
     PlayerGreen2,
     AggressiveTitForTaTeam6,
     Team1DarkRed,
+    Prescient,
+    Fucker,
+    Sane,
     ]
